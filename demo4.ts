bd2e55ac7f58f6a7ea61505cbc7ffac9ceff75c4
273* <keita.y310@icloud.com>
2024-12-24 18:43:41 +0900

    🐛 Demo4を修正しました．

--- demo4.ts
+++ demo4.ts
@@ -1,17 +1,16 @@
 // demo4.ts
 
-const somePromise = (num: number): Promise<string> =>
-  new Promise((resolve, reject) => {
-    console.log(`入力：${num}`);
-    if (num >= 10) {
-      resolve("10以上です．順：");
-    } else {
-      reject("10より小さいです．順：");
-    }
-  });
+const somePromise = async (num: number): Promise<string> => {
+  console.log(`入力：${num}`);
+  if (num >= 10) {
+    return "10以上です．順："
+  } else {
+    throw "10より小さいです．順：";
+  }
+};
 
 const processWithAwait = async (num: number) => {
-  let val: string = ""; // 結果を格納する変数
+  let val: string; // 結果を格納する変数
 
   try {
     // somePromise を実行し、成功した場合に値を取得
@@ -29,9 +28,9 @@ const processWithAwait = async (num: number) => {
     // 処理 D
     val += "D";
   } finally {
-    console.log(val);
     console.log("結果");
+    console.log(val);
   }
 };
 
-processWithAwait(12);
+processWithAwait(⬜️);
